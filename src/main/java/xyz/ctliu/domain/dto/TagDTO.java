157dc96e4e369e61320fc033f971eb969f16cc64
ctliu <yourhierarch@gmail.com>
2025-05-17 16:30:05 +0800

    feat(DTO:Tag) 文章的标签表结构

---
+++ src/main/java/xyz/ctliu/domain/dto/TagDTO.java
@@ -0,0 +1,27 @@
+package xyz.ctliu.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.hibernate.validator.constraints.Length;
+import xyz.ctliu.domain.BaseData;
+
+/**
+ * @author ctliu
+ * <p>
+ * 创建时间：2024/1/4 10:42
+ */
+
+/**
+ * 文章标签
+ */
+@Accessors(chain = true)
+@Data
+public class TagDTO implements BaseData {
+    //标签id
+    private Long id;
+    //标签名称
+    @NotBlank(message = "标签名称不能为空")
+    @Length(max = 20, message = "标签名称长度不能超过20")
+    private String tagName;
+}
